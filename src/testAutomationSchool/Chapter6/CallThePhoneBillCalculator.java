35cfdc6cdf2e28525225d528673bb266e40a529c
virtuale <okmijn0001@ya.ru>
2021-01-16 11:58:05 +0300

    update my progress

---
+++ src/testAutomationSchool/Chapter6/CallThePhoneBillCalculator.java
@@ -0,0 +1,8 @@
+package testAutomationSchool.Chapter6;
+
+public class CallThePhoneBillCalculator {
+    public static void main(String[] args) {
+        PhoneBillCalculator2 phoneBill = new PhoneBillCalculator2(1, 150, 100, 350);
+        phoneBill.resultOfAll();
+    }
+}
